1beb472bd9de1aedbb88ceaa62a9218a79b94b0f
JacobAZimmerman <ijacobzim@gmail.com>
2018-02-19 10:03:36 -0800

    problem 4

--- set1.js
+++ set1.js
@@ -1,7 +1,9 @@
-//3
+//4
 
 function whoAmI(name, age) {
     console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+    const yearOfBirth = 2018-age;
+    console.log(`I was born in ${yearOfBirth}`);
 }
 
 whoAmI('Dylan', 23);
