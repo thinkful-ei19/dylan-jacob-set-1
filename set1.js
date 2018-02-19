47080b6d1d2397b64b4f01ee05d330c049f8fee5
JacobAZimmerman <ijacobzim@gmail.com>
2018-02-19 11:10:13 -0800

    problem 8

--- set1.js
+++ set1.js
@@ -1,23 +1,25 @@
-//7
+//8
 
 function yearOfBirth(age) {
     try {let yob = 2018 - age;
-        if (yob > 2018) throw new Error("Not a valid age!")}
+        if (yob > 2018) throw new Error("Not a valid age!")
+    return yob;}
     catch (e) {
         console.log(e);
     }
 }
 
 function whoAmI(name, age) {
-
+    if (typeof name !== "string" || typeof age !== "number") {
+        console.log("Arguments not valid!");
+    }
     if (!name || !age) {
         console.error("Arguments not valid");
     } else {
         const yob = yearOfBirth(age);
-        console.log(`Hi, my name is ${name} and I'm ${yob} years old`);
-        const yearOfBirth = 2018-age;
-        console.log(`I was born in ${yearOfBirth}`);
+        console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+        console.log(`I was born in ${yob}`);
     }
 }
 
-whoAmI('Dylan', 23);
+whoAmI(124, "23");
