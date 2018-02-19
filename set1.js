d3d1b1071d59b16374600f2a28699015dc32210b
Dylan Stratton <dm.stratton94@gmail.com>
2018-02-19 12:06:17 -0600

    problem 5

--- set1.js
+++ set1.js
@@ -1,7 +1,12 @@
-//4
+//5
+
+function yearOfBirth(age) {
+    return 2018 - age;
+}
 
 function whoAmI(name, age) {
-    console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+    const yob = yearOfBirth(age);
+    console.log(`Hi, my name is ${name} and I'm ${yob} years old`);
     const yearOfBirth = 2018-age;
     console.log(`I was born in ${yearOfBirth}`);
 }
